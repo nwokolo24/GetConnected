92dc390e21a83dc69a37e099de5749b971c73bc2
nwokolo <nwokolobueze@gmail.com>
2021-09-17 21:00:18 -0600

    Section 1 - 6

--- README.md
+++ README.md
@@ -20,6 +20,19 @@ FRONT-END
 The package.json file has a list of all dependencies required for full functionalities of your front end angular application.
 -- Running "npm install" while in the client folder will install all those packages.
 
+CLI command used for creating components.
+Use "ng g -h" to see options
+
+Note: you will need to change directory into this location "/client/src/app" before running the command:
+'ng g c "name of component" --skip-tests'
+I used "--skip-tests" because this project was not built using a test driven approach but that should be the best approach.
+
+CLI command used for creating angular services
+'ng g s "name of service" --skip-tests'
+
+
+
+
 USING NUGET GALLERY IN VS CODE
 The API.csproj file has a comprehensive list of all the packages used to achieve important functionalities in the back-end
 
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public accuntServices: AccountService, private router: Router, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    this.accuntServices.login(this.model).subscribe(response => {
+      this.router.navigateByUrl("/members");
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    })
+  }
+
+  logout() {
+    this.accuntServices.logout();
+    this.router.navigateByUrl("/");
+  }
+
+}
